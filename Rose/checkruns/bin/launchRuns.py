874236a48f84dacde7cbbf0143592d7f68a28523
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-01-28 11:14:27 +0000

    start run launch script

---
+++ Rose/checkruns/bin/launchRuns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('run', nargs='+', help="runs to launch")
+    args = parser.parse_args()
+
+    for r in args.run:
+        print(r)
+
+
+if __name__ == '__main__':
+    main()
