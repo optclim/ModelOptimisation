7014d626164fa69a2460592adc103172b7cd0178
Mike Mineter <mjm@pumatest.nerc.ac.uk>
2022-02-01 12:57:20 +0000

    opt1 is a test suite seekign to get broadcast to work launchruns.sh is a script to run frmo developments of opt1, or from the ssh coomand when puma is inside Archer2 as regards AuthN and AuthZ

---
+++ Rose/onPUMA/launchRuns.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# launch new suites
+# called in short term from pumatest cylc that gets requests for new OPtCLim runs.
+# will then become with new PUMA, an ssh call
+
+OPTCLIM_STUDY_DIR=$1
+shift
+BASE_SUITE=$1
+shift
+while (($#)) ; do
+
+   #...
+     ARUN = $1
+     shift
+
+     cd /home/$USER/roses
+
+     new_suite=${ARUN}_${BASE_SUITE}
+
+     if [[ -d $new_suite ]]
+     then
+          # force uniqueness - might be unnecessary - consider later!
+          # $$ is hte process number of this process. Could use _1,_2 etc
+
+             new_suite=${new_suite}_$$
+     fi
+
+     echo  cp -r $BASE_SUITE  $NEW_SUITE
+
+     cd $new_suite
+
+          # remove lines beginning with OPTCLIM from rose-suite.conf
+     mv rose-suite.conf orig-rose-suite.conf
+     sed '/^OPTCLIM/d' orig-rose-suite.conf > rose-suite.conf
+
+          # then add the ones we want.
+     echo OPTCLIM_STUDY_DIR=${OPTCLIM_STUDY_DIR}  >> rose-suite.conf
+     echo OPTCLIM_RUNDIR=${OPTCLIM_RUNDIR}  >> rose-suite.conf
+     echo OPTCLIM_RUN=${OPTCLIM_RUN}  >> rose-suite.conf
+
+     echo in new suite $new_suite made following differences:
+     diff orig-rose-suite.conf rose-suite.conf 
+
+     echo rose suite-run --no-gcontrol
+
+     cd /home/$USER/roses
+done
