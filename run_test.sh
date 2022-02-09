37af286555b071a714f23e81515e195a877796b8
Magnus Hagdorn <Magnus.Hagdorn@marsupium.org>
2022-02-09 15:49:51 +0000

    add option to set working dir from command line

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,8 @@
 export OPTCLIMTOP=$(realpath $(dirname $0))
 export PYTHONPATH=$OPTCLIMTOP/:$OPTCLIMTOP/OptClimVn2:$OPTCLIMTOP/tools/optFunctions
 
-WORK_DIR=$(mktemp -p /tmp -d optclim_XXXX)
+
+WORK_DIR=$(mktemp -p ${1:-/tmp} -d optclim_XXXX)
 
 python tools/runAlgorithm.py --restart  --test -d $WORK_DIR Configurations/dfols14param.json
 
