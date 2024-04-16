468fa3946e1fe085e6daa7387ff9d94ad4e45fcd
not populated <m.mineter@ed.ac.uk>
2024-04-16 21:15:45 +0100

    tidy up comment

--- UKESM/simobs_dummy.sh
+++ UKESM/simobs_dummy.sh
@@ -4,8 +4,8 @@ echo in simobs_dummy.sh for crude initial test of UKESM-OptClim workflow
 echo workign directory is now:
 pwd
 
-# need to have the run directory.... not coded the context for this.
-# also need to have link from rundir to the suitedir.
+# just rename the run's paramters to be simulated observables
+
 cp runParams.json observations.json
 sed -i -e "s/iau_nontrop_max_p/iau_nontrop_max_p_tst/" observations.json
 sed -i -e "s/diagcloud_qn_compregimelimit/diagcloud_qn_compregimelimit_tst/" observations.json
